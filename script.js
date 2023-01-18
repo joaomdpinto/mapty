448fe87723daaaede71493fe1077fedc4c3c4765
João Pinto <joaomdiaspinto@gmail.com>
2023-01-18 20:45:19 +0000

    Code refactor, using OOP.

--- script.js
+++ script.js
@@ -11,75 +11,77 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
 
-navigator.geolocation.getCurrentPosition(
-    function(position){
+class App {
+    #map;
+    #mapEvent;
+
+    constructor(){
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleElevationField);
+    }
+
+    _getPosition(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(
+                this._loadMap.bind(this),
+                function(){ 
+                    alert('Could not get your position! 😞')
+                }
+            );
+        }
+    }
+
+    _loadMap(position){
         const {latitude, longitude} = position.coords;
         const coords = [latitude, longitude];
 
-        map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(this.#map);
         
-        L.marker(coords).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
+        // L.marker(coords).addTo(this.#map)
+        //     .bindPopup('A tua localização 📍')
+        //     .openPopup();
 
         //Handling clicks on map
-        map.on('click', function(mapE){
-            mapEvent = mapE;
-            form.classList.remove('hidden');
-            inputDistance.focus();
-        });
-        
-    },
-    function(){ 
-        alert('Could not get your position! 😞')
+        this.#map.on('click', this._showForm.bind(this));
     }
-);
 
-//CLEAR INPUT FIELDS
-function cleanInputFields(){
-     inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+    _showForm(mapE){
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField(){
+        inputElevation.closest('div.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('div.form__row').classList.toggle('form__row--hidden');
+    }
+    
+    _newWorkout(e){
+        e.preventDefault();
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+        const pointCoords = [this.#mapEvent.latlng.lat, this.#mapEvent.latlng.lng];
+    
+        L.marker(pointCoords).addTo(this.#map)
+        .bindPopup(
+            L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            })
+        )
+        .setPopupContent('Workout!')
+        .openPopup();       
+    }
 }
 
-//After insert data, when press "Enter", put marker and popup on de map
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    //console.log(mapEvent);
-    const pointCoords = [mapEvent.latlng.lat, mapEvent.latlng.lng];
-
-    L.marker(pointCoords).addTo(map)
-    .bindPopup(
-        L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup'
-        })
-    )
-    .setPopupContent('Workout!')
-    .openPopup();
-
-    cleanInputFields();
-});
-
-
-inputType.addEventListener('change', (e) => {
-    // if(e.target.value==='running'){
-    //     inputElevation.closest('div.form__row').classList.add('form__row--hidden');
-    //     inputCadence.closest('div.form__row').classList.remove('form__row--hidden');
-    // } 
-    // else if(e.target.value==='cycling') {
-    //     inputCadence.closest('div.form__row').classList.add('form__row--hidden');
-    //     inputElevation.closest('div.form__row').classList.remove('form__row--hidden');
-    // }
-    inputElevation.closest('div.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('div.form__row').classList.toggle('form__row--hidden');
-});
-
+const app = new App();
 
